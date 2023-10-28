a1e3579ee0de247645942b383178c579ba91fe8d
ShaliaRamiah <shalia.r20@gmail.com>
2023-10-28 23:43:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    var darkTheme = document.getElementById('dark-theme');
+    var lightTheme = document.getElementById('light-theme');
+
+    darkTheme.toggleAttribute('disabled');
+    lightTheme.toggleAttribute('disabled');
+});
+
+
+$('.back').hide();
+
+//.delay(3000)
+
+$('.front', '.flashcard').hover(function() {
+    $(this).hide();
+    $(this).siblings('.back').addClass( "animated flipInY fast" ).show();
+});
+
+$('.back', '.flashcard').mouseleave(function() {
+  $(this).hide();
+  $(this).siblings('.front').addClass( "animated flipInY fast" ).show();
+});
